0cc1a53a9c8f33d319fce1ecda1a51b24dd151f6
mchogan7 <mills.c.hogan@gmail.com>
2017-02-11 17:15:08 -0600

    initial file structure is setup

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM burgers_db.burgers
+LIMIT 0, 1000
+
+-- Date: 2017-02-11 16:53
+*/
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`burger_date`) VALUES (1,'Bacon Cheddar Burger',0,NULL);
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`burger_date`) VALUES (2,'Chicken Ranch Burger',0,NULL);
+INSERT INTO `burgers` (`id`,`burger_name`,`devoured`,`burger_date`) VALUES (3,'Gator Burger',0,NULL);
